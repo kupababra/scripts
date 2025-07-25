735a0f2fba05fb3fb608ac1b9a4c6d0e1071af13
abrakadabra <c3z4r3k@gmail.com>
2025-07-25 13:48:28 +0200

    Add files via upload

---
+++ szukaj.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Skrypt do wyszukiwania pakietów w Gentoo
+# bofh@retro-technology.pl
+
+if [ -z "$1" ]; then
+  echo "Podaj nazwę pakietu do wyszukania."
+  exit 1
+fi
+
+# Zmienna z nazwą pakietu
+PACKAGE_NAME=$1
+
+# Wyszukiwanie pakietu w portage
+echo "Wyszukiwanie pakietu: $PACKAGE_NAME..."
+emerge -s $PACKAGE_NAME
+
+# Alternatywnie, jeśli chcesz używać equery do szczegółowych informacji
+echo "Szczegóły pakietu:"
+equery l $PACKAGE_NAME
+
